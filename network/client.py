21b540d3cdb26571b23a1d8d0cdff692c88a0363
KeremAltay59 <ke.altay147@gmail.com>
2026-03-27 14:16:08 +0300

    hafta 4

--- network/client.py
+++ network/client.py
@@ -1,6 +1,7 @@
 import socket
+import threading
 
-SERVER_IP = "127.0.0.1"  # kendi bilgisayarın
+SERVER_IP = "127.0.0.1"
 PORT = 5000
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,10 +9,30 @@ client.connect((SERVER_IP, PORT))
 
 print("[BAĞLANDI]")
 
-while True:
-    msg = input("Mesaj yaz: ")
 
-    client.send(msg.encode())
+# 🔹 Mesaj alma
+def receive_messages():
+    while True:
+        try:
+            data = client.recv(1024)
+            if not data:
+                break
+            print(f"\n[SERVER]: {data.decode()}")
+        except:
+            break
 
-    data = client.recv(1024)
-    print(f"[SERVER] {data.decode()}")
+
+# 🔹 Mesaj gönderme
+def send_messages():
+    while True:
+        try:
+            name = input("ADINIZ : ")
+            msg = input()
+            client.send(f"{name} : {msg}".encode())
+        except:
+                 print("Bağlantı Koptu!")
+                 break
+
+
+threading.Thread(target=receive_messages).start()
+threading.Thread(target=send_messages).start()
--- network/server.py
+++ network/server.py
@@ -1,25 +1,37 @@
 import socket
+import threading
 
-HOST = "0.0.0.0"   # herkes bağlanabilsin
+HOST = "127.0.0.1"
 PORT = 5000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-
 server.listen()
 
 print(f"[SERVER] Dinleniyor: {HOST}:{PORT}")
 
 conn, addr = server.accept()
-print(f"[BAĞLANTI GELDİ] {addr}")
+print(f"[BAĞLANDI] {addr}")
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
 
-    print(f"[MESAJ] {data.decode()}")
+# 🔹 Mesaj alma thread'i
+def receive_messages():
+    while True:
+        try:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"\n[CLIENT]: {data.decode()}")
+        except:
+            break
 
-    conn.send("Mesaj alındı".encode())
 
-conn.close()
+# 🔹 Mesaj gönderme thread'i
+def send_messages():
+    while True:
+        msg = input()
+        conn.send(msg.encode())
+
+# Thread başlat
+threading.Thread(target=receive_messages).start()
+threading.Thread(target=send_messages).start()
